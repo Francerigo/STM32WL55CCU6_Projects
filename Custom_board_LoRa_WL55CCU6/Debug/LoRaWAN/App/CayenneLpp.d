3bc8418117fcf21ab34663ad05bd793bbba1a1d7
Francerigo <fr.rigo2000@gmail.com>
2025-12-09 16:20:58 +0100

    Add folder

---
+++ Custom_board_LoRa_WL55CCU6/Debug/LoRaWAN/App/CayenneLpp.d
@@ -0,0 +1,14 @@
+LoRaWAN/App/CayenneLpp.o: ../LoRaWAN/App/CayenneLpp.c \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/misc/stm32_mem.h \
+ ../Core/Inc/utilities_conf.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/misc/stm32_tiny_vsnprintf.h \
+ ../Core/Inc/utilities_def.h ../LoRaWAN/App/CayenneLpp.h
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/misc/stm32_mem.h:
+../Core/Inc/utilities_conf.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/misc/stm32_tiny_vsnprintf.h:
+../Core/Inc/utilities_def.h:
+../LoRaWAN/App/CayenneLpp.h:
