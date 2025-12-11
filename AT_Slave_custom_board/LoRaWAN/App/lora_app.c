49c54bae2106283e4d728cadfc8d19b07ef60c43
Francerigo <fr.rigo2000@gmail.com>
2025-12-11 12:49:56 +0100

    prova

--- AT_Slave_custom_board/LoRaWAN/App/lora_app.c
+++ AT_Slave_custom_board/LoRaWAN/App/lora_app.c
@@ -35,7 +35,7 @@
 #include "flash_if.h"
 
 /* USER CODE BEGIN Includes */
-
+//prova
 /* USER CODE END Includes */
 
 /* External variables ---------------------------------------------------------*/
