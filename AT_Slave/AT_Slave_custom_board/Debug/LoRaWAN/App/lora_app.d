42a3f3f89e89d10072c6af352d3ae50b31702f52
Francerigo <fr.rigo2000@gmail.com>
2025-12-11 12:53:50 +0100

    prova

---
+++ AT_Slave/AT_Slave_custom_board/Debug/LoRaWAN/App/lora_app.d
@@ -0,0 +1,164 @@
+LoRaWAN/App/lora_app.o: ../LoRaWAN/App/lora_app.c ../Core/Inc/platform.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Device/ST/STM32WLxx/Include/stm32wlxx.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Device/ST/STM32WLxx/Include/stm32wl55xx.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Device/ST/STM32WLxx/Include/system_stm32wlxx.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal.h \
+ ../Core/Inc/stm32wlxx_hal_conf.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_dma.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_def.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_dma.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_dmamux.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_dma_ex.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_adc.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_adc.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_adc_ex.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_cortex.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_exti.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_flash.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_flash_ex.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_gpio.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_gpio_ex.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_pwr.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_pwr.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_pwr_ex.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_rcc.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_rcc.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_bus.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_rcc_ex.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_exti.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_rtc.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_rtc_ex.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_subghz.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_spi.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_uart.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_uart_ex.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_system.h \
+ ../Core/Inc/main.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_gpio.h \
+ ../Drivers/BSP/STM32WLxx_Nucleo/stm32wlxx_nucleo_radio.h \
+ ../Drivers/BSP/STM32WLxx_Nucleo/stm32wlxx_nucleo_errno.h \
+ ../Core/Inc/stm32wlxx_nucleo_conf.h \
+ ../Drivers/BSP/STM32WLxx_Nucleo/stm32wlxx_nucleo.h ../Core/Inc/sys_app.h \
+ ../Core/Inc/sys_conf.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/trace/adv_trace/stm32_adv_trace.h \
+ ../Core/Inc/utilities_conf.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/misc/stm32_mem.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/misc/stm32_tiny_vsnprintf.h \
+ ../Core/Inc/utilities_def.h ../LoRaWAN/App/lora_app.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/sequencer/stm32_seq.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/timer/stm32_timer.h \
+ ../Core/Inc/utilities_def.h ../LoRaWAN/App/app_version.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/LmHandler/lorawan_version.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/SubGHz_Phy/stm32_radio_driver/subghz_phy_version.h \
+ ../LoRaWAN/App/lora_info.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/LmHandler/LmHandler.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/LmHandler/LmHandlerTypes.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacInterfaces.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacTypes.h \
+ ../LoRaWAN/Target/timer.h ../LoRaWAN/Target/systime.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/misc/stm32_systime.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacVersion.h \
+ ../LoRaWAN/Target/lorawan_conf.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/Region/RegionNvm.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacTypes.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/Region/RegionVersion.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacVersion.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacCryptoNvm.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Utilities/utilities.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/secure-element-nvm.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacClassBNvm.h \
+ C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/Region/RegionVersion.h \
+ ../LoRaWAN/App/lora_command.h ../LoRaWAN/App/lora_at.h \
+ ../Core/Inc/flash_if.h ../Core/Inc/platform.h
+../Core/Inc/platform.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Device/ST/STM32WLxx/Include/stm32wlxx.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Device/ST/STM32WLxx/Include/stm32wl55xx.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/mpu_armv7.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Device/ST/STM32WLxx/Include/system_stm32wlxx.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal.h:
+../Core/Inc/stm32wlxx_hal_conf.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_dma.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_def.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_dma.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_dmamux.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_dma_ex.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_adc.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_adc.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_adc_ex.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_cortex.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_exti.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_flash.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_flash_ex.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_gpio.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_gpio_ex.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_pwr.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_pwr.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_pwr_ex.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_rcc.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_rcc.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_bus.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_rcc_ex.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_exti.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_rtc.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_rtc_ex.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_subghz.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_spi.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_uart.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_hal_uart_ex.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_system.h:
+../Core/Inc/main.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/STM32WLxx_HAL_Driver/Inc/stm32wlxx_ll_gpio.h:
+../Drivers/BSP/STM32WLxx_Nucleo/stm32wlxx_nucleo_radio.h:
+../Drivers/BSP/STM32WLxx_Nucleo/stm32wlxx_nucleo_errno.h:
+../Core/Inc/stm32wlxx_nucleo_conf.h:
+../Drivers/BSP/STM32WLxx_Nucleo/stm32wlxx_nucleo.h:
+../Core/Inc/sys_app.h:
+../Core/Inc/sys_conf.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/trace/adv_trace/stm32_adv_trace.h:
+../Core/Inc/utilities_conf.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/misc/stm32_mem.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/misc/stm32_tiny_vsnprintf.h:
+../Core/Inc/utilities_def.h:
+../LoRaWAN/App/lora_app.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/sequencer/stm32_seq.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/timer/stm32_timer.h:
+../Core/Inc/utilities_def.h:
+../LoRaWAN/App/app_version.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/LmHandler/lorawan_version.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/SubGHz_Phy/stm32_radio_driver/subghz_phy_version.h:
+../LoRaWAN/App/lora_info.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/LmHandler/LmHandler.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/LmHandler/LmHandlerTypes.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacInterfaces.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacTypes.h:
+../LoRaWAN/Target/timer.h:
+../LoRaWAN/Target/systime.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Utilities/misc/stm32_systime.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacVersion.h:
+../LoRaWAN/Target/lorawan_conf.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/Region/RegionNvm.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacTypes.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/Region/RegionVersion.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacVersion.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacCryptoNvm.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Utilities/utilities.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/secure-element-nvm.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/LoRaMacClassBNvm.h:
+C:/Users/LabMeas/STM32Cube/Repository/STM32Cube_FW_WL_V1.3.1/Middlewares/Third_Party/LoRaWAN/Mac/Region/RegionVersion.h:
+../LoRaWAN/App/lora_command.h:
+../LoRaWAN/App/lora_at.h:
+../Core/Inc/flash_if.h:
+../Core/Inc/platform.h:
